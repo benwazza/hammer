2487426d89e23c71533ee8b2648d6e26c96c0cb4
benwazza <ben@warren.net.au>
2009-06-02 23:29:26 +1000

    Updated header copyright dates

--- src/self/hammer/self/Compile.java
+++ src/self/hammer/self/Compile.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008 Ben Warren
+ *  Copyright 2008-2009 Ben Warren
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
